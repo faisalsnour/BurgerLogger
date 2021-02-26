ad2ea8e822f70068e9200672fcfe0e6b78162d73
Faisal Nour <faisalskhalifa@gmail.com>
2021-02-25 23:44:32 -0500

    connection file has been updated

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,19 @@
 const mysql = require( 'mysql' )
+var connection;
+
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'hacktheplanet',
+        database: 'todoagain_db'
+    });
+}
+
+connection.connect();
+module.exports = connection;
 
 // use this wrapper to create promise around mysql
 class Database {
@@ -25,6 +40,9 @@ class Database {
         } );
     }
 }
+
+
+
 
 function dbConnect( dbName, dbPassword ){
     // at top INIT DB connection
